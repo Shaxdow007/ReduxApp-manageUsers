5a2a616c4d1df1cd8c00cbdefc986e026b5171b5
shadow007 <nourdinehoumame0@gmail.com>
2024-01-09 19:53:46 +0100

    config link with router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserList from "./Components/UserList";
+import AddUser from "./Components/AddUser";
+import UpdateUser from "./Components/UpdateUser";
 
 const App = () => {
-  return <div className="container">
-    
-  </div>;
+  return (
+    <div className="container my-5">
+      <h1>CRUD REACTJS WITH REDUX</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={UserList} />
+          <Route path="/add-user" element={AddUser} />
+          <Route path="/update-user/:id" element={UpdateUser} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
